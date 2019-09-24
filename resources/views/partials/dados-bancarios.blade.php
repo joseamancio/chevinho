f4a7a0770552c8d618322ca44d9e734860685320
Amancio <amancio@sportmanager365.com>
2019-09-24 12:57:59 -0300

    add picpay

--- resources/views/partials/dados-bancarios.blade.php
+++ resources/views/partials/dados-bancarios.blade.php
@@ -1,62 +1,75 @@
 <section class="dados-bancarios">
-            <h3 class="page-section-heading text-center text-secundary">Dados Bancários</h3>
-            <br>
-            <div class="row">
-                <div class="col-sm-6 col-12 clearfix">
-                    <div class="row">
-                        <div class="col-sm-4 col-4">
-                            <img src="{{asset('img/Banco.png')}}" class="img-responsive" alt="Banco do Brasil">
-                        </div>
-                        <div class="col-sm-8">
-                            <p><b>Titular:</b> José Amâncio de Moraes Júnior</p>
-                            <p><b>CPF:</b> 128.192.866-66</p>
-                            <p><b>Agência:</b> 2930-0</p>
-                            <p><b>Conta Corrente:</b> 23921-6</p>
-                        </div>
+    <div class="container">
+        <h3 class="page-section-heading text-center text-secundary">Dados Bancários</h3>
+        <br>
+        <div class="row">
+            <div class="col-sm-6 col-12 clearfix">
+                <div class="row">
+                    <div class="col-sm-4 col-4">
+                        <img src="{{asset('img/Banco.png')}}" class="img-responsive" alt="Banco do Brasil">
                     </div>
-                </div>
-                <div class="col-sm-6 col-12 clearfix">
-                    <div class="row">
-                        <div class="col-sm-4 col-4">
-                            <img src="{{asset('img/caixa.jpg')}}" class="img-responsive" alt="Banco do Brasil">
-                        </div>
-                        <div class="col-sm-8">
-                            <p><b>Titular:</b> Renato Tadeu Lee Dragunskis Moreira</p>
-                            <p><b>CPF:</b> 094.878.106-84</p>
-                            <p><b>Agência:</b> 0084</p>
-                            <p><b>Operação:</b> 013</p>
-                            <p><b>Conta Poupança:</b> 23338-2</p>
-                        </div>
+                    <div class="col-sm-8">
+                        <p><b>Titular:</b> José Amâncio de Moraes Júnior</p>
+                        <p><b>CPF:</b> 128.192.866-66</p>
+                        <p><b>Agência:</b> 2930-0</p>
+                        <p><b>Conta Corrente:</b> 23921-6</p>
                     </div>
                 </div>
             </div>
-            <br>
-            <div class="row row-dados-bancarios">
-                <div class="col-sm-6 col-12 clearfix">
-                    <div class="row">
-                        <div class="col-sm-4 col-4">
-                            <img src="{{asset('img/Itau.png')}}" class="img-responsive" alt="Banco do Brasil">
-                        </div>
-                        <div class="col-sm-8">
-                            <p><b>Titular:</b> José Amâncio de Moraes Júnior</p>
-                            <p><b>CPF:</b> 128.192.866-66</p>
-                            <p><b>Agência:</b> 6943</p>
-                            <p><b>Conta Corrente:</b> 16309-4</p>
-                        </div>
+            <div class="col-sm-6 col-12 clearfix">
+                <div class="row">
+                    <div class="col-sm-4 col-4">
+                        <img src="{{asset('img/caixa.jpg')}}" class="img-responsive" alt="Banco do Brasil">
                     </div>
-                </div>
-                <div class="col-sm-6 col-12 clearfix">
-                    <div class="row">
-                        <div class="col-sm-4 col-4">
-                            <img src="{{asset('img/Banco-Inter.png')}}" class="img-responsive" alt="Banco do Brasil">
-                        </div>
-                        <div class="col-sm-8">
-                            <p><b>Titular:</b> José Amâncio de Moraes Júnior</p>
-                            <p><b>CPF:</b> 128.192.866-66</p>
-                            <p><b>Agência:</b> 0001</p>
-                            <p><b>Conta Corrente:</b> 2176641-0</p>
-                        </div>
+                    <div class="col-sm-8">
+                        <p><b>Titular:</b> Renato Tadeu Lee Dragunskis Moreira</p>
+                        <p><b>CPF:</b> 094.878.106-84</p>
+                        <p><b>Agência:</b> 0084</p>
+                        <p><b>Operação:</b> 013</p>
+                        <p><b>Conta Poupança:</b> 23338-2</p>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+        <div class="row row-dados-bancarios">
+            <div class="col-sm-6 col-12 clearfix">
+                <div class="row">
+                    <div class="col-sm-4 col-4">
+                        <img src="{{asset('img/Itau.png')}}" class="img-responsive" alt="Banco do Brasil">
+                    </div>
+                    <div class="col-sm-8">
+                        <p><b>Titular:</b> José Amâncio de Moraes Júnior</p>
+                        <p><b>CPF:</b> 128.192.866-66</p>
+                        <p><b>Agência:</b> 6943</p>
+                        <p><b>Conta Corrente:</b> 16309-4</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-12 clearfix">
+                <div class="row">
+                    <div class="col-sm-4 col-4">
+                        <img src="{{asset('img/Banco-Inter.png')}}" class="img-responsive" alt="Banco do Brasil">
+                    </div>
+                    <div class="col-sm-8">
+                        <p><b>Titular:</b> José Amâncio de Moraes Júnior</p>
+                        <p><b>CPF:</b> 128.192.866-66</p>
+                        <p><b>Agência:</b> 0001</p>
+                        <p><b>Conta Corrente:</b> 2176641-0</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row row-dados-bancarios">
+            <div class="col-sm-6 col-12 clearfix">
+                <div class="row">
+                    <div class="col-sm-4 col-4">
+                        <img src="{{asset('img/picpay.png')}}" class="img-responsive" alt="Banco do Brasil">
+                    </div>
+                    <div class="col-sm-8">
+                        <p><b>Conta:</b>@renatodragunskis</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
